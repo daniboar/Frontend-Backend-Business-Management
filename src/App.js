cc6119e044adbf234bc0437fac578787befbbc68
MonkeYzPGL <elecfis@gmail.com>
2023-11-22 14:42:16 +0200

    LogIn page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginForm from './LogIn';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Project Coming soon...Bussiness Management
-        </p>
-        <p>
-          Owners: Boar Daniel & Elecfi Sergiu & Maghiar Antoniu
-        </p>
-      </header>
+      <LoginForm />
     </div>
   );
 }
---
+++ src/LogIn/Login.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Login.css';
+
+const LoginForm = () => {
+  return (
+    <div id="loginform">
+      <FormHeader title="Login" />
+      <Form />
+    </div>
+  );
+};
+
+const FormHeader = (props) => <h2 id="headerTitle">{props.title}</h2>;
+
+const Form = (props) => (
+  <div>
+    <FormInput description="Username" placeholder="Enter your username" type="text" />
+    <FormInput description="Password" placeholder="Enter your password" type="password" />
+    <FormButton title="Log in" />
+  </div>
+);
+
+const FormButton = (props) => (
+  <div id="button" className="row">
+    <button>{props.title}</button>
+  </div>
+);
+
+const FormInput = (props) => (
+  <div className="row">
+    <label>{props.description}</label>
+    <input type={props.type} placeholder={props.placeholder} />
+  </div>
+);
+
+export default LoginForm;
