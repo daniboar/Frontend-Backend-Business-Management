52f2cc8ad3f3d44c6bf67a69843c795e5f5e335a
Dani <daniboar@yahoo.com>
2024-01-03 13:08:44 +0200

    Sql

--- src/LogIn/CEO/ManagementProiecte/ManagementProiecte.jsx
+++ src/LogIn/CEO/ManagementProiecte/ManagementProiecte.jsx
@@ -5,6 +5,8 @@ import './ManagementProiecte.css';
 
 const ManagementCereri = () => {
   const [comentariu, setComentariu] = useState('');
+  const [titluCerere, setTitluCerere] = useState('');
+  const [numeProiect, setNumeProiect] = useState('');
 
   const handleAcceptare = () => {
     // Logica pentru acțiunea de acceptare
@@ -19,16 +21,47 @@ const ManagementCereri = () => {
   return (
     <div>
       <h1>Management cereri de proiect</h1>
+
+      {/* Primul câmp de text cu un text asociat */}
       <label>
-        Cereri:
+        ID Proiect: 
+        <input
+          type="text"
+          value={titluCerere}
+          onChange={(e) => setTitluCerere(e.target.value)}
+        />
+      </label>
+      <br />
+
+      {/* Al doilea câmp de text cu un text asociat */}
+      <label>
+        ID Cerere: 
+        <input
+          type="text"
+          value={numeProiect}
+          onChange={(e) => setNumeProiect(e.target.value)}
+        />
+      </label>
+      <br />
+
+      {/* Câmpul pentru comentarii (textArea) */}
+      <label>
+        Cereri de proiecte:
         <textarea
           value={comentariu}
           onChange={(e) => setComentariu(e.target.value)}
         />
       </label>
       <br />
+
+      {/* Buton pentru acceptare */}
       <button onClick={handleAcceptare}>Acceptare</button>
+
+      {/* Buton pentru respingere */}
       <button onClick={handleRespingere}>Respingere</button>
+      <button className="backButton">
+        Back
+      </button>
     </div>
   );
 };
